72d9fbb650a06a418eb82ddb0ffe6c5acfb06b6b
josefinagoni <gonij@udesa.edu.ar>
2021-04-12 13:45:24 -0300

    routes todas en index, no funciona href en vistas

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,21 @@
 const controlador = {
     index: (req, res) => {
         res.render('index', {})
+    },
+    product: (req, res) => {
+        res.render('product', {})
+    },
+    login: (req, res) => {
+        res.render('login', {})
+    },
+    register: (req, res) => {
+        res.render('register', {})
+    },
+    search: (req, res) => {
+        res.render('search', {})
+    },
+    profile: (req, res) => {
+        res.render('profile', {})
     }
 
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 let indexController = require('../controllers/indexController');
 
 router.get('/', indexController.index);
+router.get('/product', indexController.product);
+router.get('/login', indexController.login);
+router.get('/register', indexController.register);
+router.get('/search', indexController.search);
+router.get('/profile', indexController.profile);
 
 
 module.exports = router;
