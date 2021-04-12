bd828da3809c62bfec2473c3fe9303ea5df42d56
josefinagoni <gonij@udesa.edu.ar>
2021-04-12 12:55:12 -0300

    archivos y estructuras

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+let indexController = require('../controllers/indexController');
+
+router.get('/', indexController.index);
+
+
+module.exports = router;
