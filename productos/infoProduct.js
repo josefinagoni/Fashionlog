84d56e479c61b0bfc9634b6afc3e33bb5d21dcc0
milagrosjones <mjones@udesa.edu.ar>
2021-04-13 15:50:41 -0300

    infoproductos

---
+++ productos/infoProduct.js
@@ -0,0 +1,44 @@
+let listadoProducts = {
+    lista: [
+        {
+            nombre: "Campera Mango",
+            descripcion: "Campera de Jean color Azul",
+            estado: "Usada",
+            cover:""
+        },
+        {
+            nombre: "Campera Rapsodia",
+            descripcion: "Campera de cuero negra con mangas tres cuartos",
+            estado: "Usada",
+            cover:""
+        },
+        {
+            nombre:"Pantalon Zara",
+            descripcion: "Pantalon de cuero original chupin",
+            estado: "Casi nuevo",
+            cover:""
+        },
+        {
+            nombre: "Campera Zara",
+            descripcion:"Campera de Jean color negra",
+            estado: "Nueva con etiqueta",
+            cover:""
+        },
+        {
+       
+            nombre: "Remera Forever XXI",
+            descripcion: "Remera negra basica",
+            estado: "Nueva con etiqueta",
+            cover: ""
+        },
+        {
+            nombre: "Jean Abercrombie",
+            descripcion:"Jean azul recto con corte en rodilla",
+            estado:"Nuevo sin etiqueta",
+            cover:""
+        },
+       
+    ]
+}
+
+module.exports = listadoProducts;
