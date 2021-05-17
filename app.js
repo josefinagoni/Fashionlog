c2dfb2a858ac200eb507357aaa01e3fd2626d8e2
milagrosjones <mjones@udesa.edu.ar>
2021-05-17 09:31:41 -0300

    session instalado

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//agregado session
+const session = require('express-session');
+
+app.use(session({secret: "",
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use('/index', indexRouter);
 
 
--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "db.sql",
+    "database": "proyectofashionlog",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
