9874633a4f87d2853fc0f6670bf644ebae3c2a00
josefinagoni <gonij@udesa.edu.ar>
2021-06-15 17:17:20 -0300

    cambiando modelo usuario

--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -30,6 +30,9 @@ module.exports = (sequelize, dataTypes) => {
         },
         updatedAt: {
             type: dataTypes.DATE
+        },
+        imagen: {
+            type: dataTypes.STRING
         }
             
         
