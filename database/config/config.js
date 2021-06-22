7cb146db0a1a032bcee4f9da9a1c7c5a88acc4e2
Agusbierfass <agusbierfass@gmail.com>
2021-06-22 15:10:14 -0300

    m

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "root",
     "database": "FASHIONLOG_Trabajo",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "3306",
+    "port": "8889",
   }
 }
 
