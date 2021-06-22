01ee1a663b65f813c5afd6de8811b15db64f4e1e
josefinagoni <gonij@udesa.edu.ar>
2021-06-22 16:02:21 -0300

    no existe product

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -17,9 +17,16 @@ module.exports = {
         
         
         db.Producto.findAll(buscar).then(resultado => {
-            res.render('search', {lista: resultado, error: null,
+            if (resultado) {
+                res.render('search', {lista: resultado, error: null,
                 usuario: resultado.usuario,
                 comentario: resultado.comentario});
+            }else{
+                res.render('search', {
+                    error: "No existe el producto "
+                });
+            }
+            
         }).catch(error => {
             console.log("Error de conexion: " + error.message);
             res.render('index', {
